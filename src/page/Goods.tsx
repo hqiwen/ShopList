9d8de41267d5316fc49b271b70ef673b608acb9d
hqiwen <1726564970@qq.com>
2019-09-13 18:17:39 +0800

    添加 post 和 comment

--- src/page/Goods.tsx
+++ src/page/Goods.tsx
@@ -1,61 +1,39 @@
-import { Card, Col, Comment, Icon, List, Row, Tooltip } from "antd/es";
-import Meta from "antd/es/card/Meta";
+import { Button } from "antd";
+import { Card, Col, Comment, Descriptions, List, Modal, Row, Tooltip } from "antd/es";
 import moment from "moment";
 import React, { useState } from "react";
 import { RouteComponentProps } from "react-router";
+import { getCurUser } from "../App";
 import Footer from "../component/footer";
 import Header from "../component/header";
 
-const data = [
+interface OrderModel {
+    "user": string,
+    "goodsName": string,
+    "sumPrice": number,
+    "goodsNumber": number
+}
+
+let order: OrderModel[] = [{
+    "user" : "default",
+    "goodsName": "A0",
+    "sumPrice": 0,
+    "goodsNumber": 0
+}];
+
+const comments = [
     {
-        author: 'Han Solo',
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        content: (
-            <p>
-                We supply a series of design principles, practical patterns and high quality design
-                resources (Sketch and Axure), to help people create their product prototypes beautifully and
-                efficiently.
-      </p>
-        ),
-        datetime: (
-            <Tooltip
-                title={moment()
-                    .subtract(1, 'days')
-                    .format('YYYY-MM-DD HH:mm:ss')}
-            >
-                <span>
-                    {moment()
-                        .subtract(1, 'days')
-                        .fromNow()}
-                </span>
-            </Tooltip>
-        ),
-    },
-    {
-        author: 'Han Solo',
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        content: (
-            <p>
-                We supply a series of design principles, practical patterns and high quality design
-                resources (Sketch and Axure), to help people create their product prototypes beautifully and
-                efficiently.
-      </p>
-        ),
-        datetime: (
-            <Tooltip
-                title={moment()
-                    .subtract(2, 'days')
-                    .format('YYYY-MM-DD HH:mm:ss')}
-            >
-                <span>
-                    {moment()
-                        .subtract(2, 'days')
-                        .fromNow()}
-                </span>
-            </Tooltip>
-        ),
-    },
-];
+        "user": "Han Solo",
+        "avatar": "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        "content": "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+        "updateAt": new Date("2019-9-24")
+    } , {
+        "user": "Jack",
+        "avatar": "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        "content": "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+        "updateAt": new Date("2019-9-24")
+    }
+]
 
 interface GoodsProps extends RouteComponentProps {
     match: {
@@ -66,9 +44,51 @@ interface GoodsProps extends RouteComponentProps {
     }
 }
 
+function postOrder(goodsName, sumPrice, goodsNumber) {
+    let curUser = getCurUser();
+    if (curUser.userId >= 0) {
+        order.push({
+            "user": curUser.userName,
+            "goodsName": goodsName,
+            "sumPrice": sumPrice,
+            "goodsNumber": goodsNumber
+        });
+        success();
+    } else {
+        loginError();
+    }
+    
+    console.log(order);
+}
+
+function getOrders() {
+    return order;
+}
+
+function getComments() {
+    return comments;
+}
+
+function loginError() {
+    Modal.error({
+        title: "用户未登录",
+        content: "在购买前请先登录你的账号"
+    })
+}
+
+function success() {
+    Modal.success({
+        title: "购买成功",
+        content: "请享受你的购买之旅"
+    })
+}
+
 const Goods: React.FC<GoodsProps> = ({ match }) => {
     const [goodsName] = useState("joke");
     const [goodsPrice] = useState(5);
+    const [discount] = useState(0.85);
+    const [buyNumber, setBuyNumber] = useState(1);
+    const sumPrice = buyNumber * discount * goodsPrice;
 
     return (
         <div>
@@ -77,7 +97,7 @@ const Goods: React.FC<GoodsProps> = ({ match }) => {
                 <h3 style={{ padding: 15 }}>this {match.params.goodsId}goods</h3>
                 <div style={{ height: 16 }}></div>
                 <Row>
-                    <Col offset={4} span={8}>
+                    <Col offset={2} span={6}>
                         <Card
                             cover={
                                 <img
@@ -85,44 +105,55 @@ const Goods: React.FC<GoodsProps> = ({ match }) => {
                                     src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
                                 />
                             }
-                            actions={[
-                                <Icon type="setting" key="setting" />,
-                                <Icon type="edit" key="edit" />,
-                                <Icon type="ellipsis" key="ellipsis" />,
-                            ]}
                         >
-                            <Meta
-                                title="商品名"
-                                description={goodsName}
-                            />
-                            <Meta
-                                title="商品价格"
-                                description={goodsPrice}
-                            />
+                            <Descriptions title="商品描述" layout="horizontal" size="middle">
+                                <Descriptions.Item label="商品名">{goodsName}</Descriptions.Item>
+                                <Descriptions.Item label="商品价格">{goodsPrice}</Descriptions.Item>
+                            </Descriptions>
                         </Card>
+                    </Col>
+                    <Col offset={4} span={6}>
+                        <h2>想要获得它</h2>
+                        <p style={{ lineHeight: "24px" }}>限时折扣: <span>{discount}</span></p>
+                        <p style={{ lineHeight: "24px" }}>购买数量: <span>{buyNumber}</span></p>
+                        <Button style={{ width: "50%" }} onClick={() => setBuyNumber(buyNumber + 1)}>添加</Button>
+                        <Button style={{ width: "50%" }} onClick={() => buyNumber < 1 ? setBuyNumber(0) : setBuyNumber(buyNumber - 1)}>减少</Button>
+                        <p style={{ lineHeight: "48px" }}>总价: <span style={{color: "red", fontSize: "24px"}}>{sumPrice}</span></p>
+                        <Button style={{ width: "100%" }} onClick={() => postOrder(goodsName, sumPrice, buyNumber)}>提交</Button>
                     </Col>
                 </Row>
                 <div style={{ height: 16 }}></div>
                 <List
                     style={{ padding: 15 }}
-                    header={`${data.length} comments`}
+                    header={`${comments.length} comments`}
                     itemLayout="horizontal"
-                    dataSource={data}
+                    dataSource={comments}
                     renderItem={item => (
                         <li>
                             <Comment
-                                author={item.author}
+                                author={item.user}
                                 avatar={item.avatar}
-                                content={item.content}
-                                datetime={item.datetime}
+                                content={<p>{item.content}</p>}
+                                datetime={<Tooltip
+                                    title={ moment(item.updateAt)
+                                        .format('YYYY-MM-DD HH:mm:ss')}
+                                >
+                                    <span>
+                                        {moment(item.updateAt)
+                                            .fromNow()}
+                                    </span>
+                                </Tooltip>
+                                }
                             />
                         </li>
                     )}
                 />
+                <Button>添加评论</Button>
             </main>
             <Footer></Footer>
         </div>
     )
 }
 
+export { getComments, getOrders };
 export default Goods;
---
+++ src/page/Space.tsx
@@ -0,0 +1,89 @@
+import { Comment, Descriptions, List } from "antd/es";
+import React from "react";
+import { Redirect } from "react-router";
+import { fakeAuth, getCurUser } from "../App";
+import Footer from "../component/footer";
+import Header from "../component/header";
+import { getComments, getOrders } from "./Goods";
+
+const Space: React.FC = () => {
+    let curUser = getCurUser();
+
+    return fakeAuth.isAuthenticated ?
+        (
+            <div>
+                <Header></Header>
+                <main>
+                    <div>
+                        <Descriptions title="个人信息" layout="horizontal" size="middle">
+                            <Descriptions.Item label="用户名">{curUser.userName}</Descriptions.Item>
+                            <Descriptions.Item label="用户密码">{curUser.userPassword}</Descriptions.Item>
+                        </Descriptions>
+                    </div>
+                    <div>
+                        <h2>我的评论</h2>
+                        <SpaceComment></SpaceComment>
+                    </div>
+                    <div>
+                        <h2>我的订单</h2>
+                        <SpaceList></SpaceList>
+                    </div>
+                </main>
+                <Footer></Footer>
+            </div>
+        ) : (
+            <Redirect to="/"></Redirect>
+        )
+}
+
+function getOwnerComments() {
+    let userName = getCurUser().userName;
+    return getComments().filter((val) => val.user === userName);
+}
+
+function getOwnerOrders() {
+    let userName = getCurUser().userName;
+    return getOrders().filter((val) => val.user === userName);
+}
+
+const SpaceComment: React.FC = () => {
+    let comments = getOwnerComments();
+    return (
+        <List
+            style={{ padding: 15 }}
+            header={`${comments.length} comments`}
+            itemLayout="horizontal"
+            dataSource={comments}
+            renderItem={item => (
+                <li>
+                    <Comment
+                        author={item.user}
+                        avatar={item.avatar}
+                        content={<p>{item.content}</p>}
+                    />
+                </li>
+            )}
+        />
+    )
+}
+
+const SpaceList: React.FC = () => {
+    let orders = getOwnerOrders();
+    return (
+        <List
+            style={{ padding: 15 }}
+            header={`${orders.length} orders`}
+            itemLayout="horizontal"
+            dataSource={orders}
+            renderItem={item => (
+                <li>
+                    <p style={{ lineHeight: "24px" }}>购买名称: <span>{item.goodsName}</span></p>
+                    <p style={{ lineHeight: "24px" }}>购买数量: <span>{item.goodsNumber}</span></p>
+                    <p style={{ lineHeight: "48px" }}>总价: <span style={{ color: "red", fontSize: "24px" }}>{item.sumPrice}</span></p>
+                </li>
+            )}
+        />
+    )
+}
+
+export default Space;
